1e139cd88d1901373c6801e0831c4067c7797d0d
u1671506 <u1671506@SB6A-33-967908.AD.HUD.AC.UK>
2017-02-14 14:15:46 +0000

    Change the snack class

--- src/Snack.java
+++ src/Snack.java
@@ -1,23 +1,43 @@
+/*
+ * 
+ */
 public class Snack {
-    private String snackName;
-    private double snackPrice;
+	private String snackName;
+	private double snackPrice;
 
-    public Snack(String snackName, double price) {
-        this.snackName = snackName;
-        this.snackPrice = price;
-    }
-    /*
-     * Gets the name of snack
-     * 
-     * @return snackName The name of the snack
-     */
-    public String getSnackName(){
-        return snackName;
-    }
-    /*
-     * @return 
-     */
-    public double getSnackPrice(){
-        return snackPrice;
-    }
+	public Snack(String snackName, double price) {
+		this.setSnackName(snackName);
+		this.setSnackPrice(price);
+	}
+
+	/**
+	 * @return the snackName
+	 */
+	public String getSnackName() {
+		return snackName;
+	}
+
+	/**
+	 * @param snackName
+	 *            the snackName to set
+	 */
+	public void setSnackName(String snackName) {
+		this.snackName = snackName;
+	}
+
+	/**
+	 * @return the snackPrice
+	 */
+	public double getSnackPrice() {
+		return snackPrice;
+	}
+
+	/**
+	 * @param snackPrice
+	 *            the snackPrice to set
+	 */
+	public void setSnackPrice(double snackPrice) {
+		this.snackPrice = snackPrice;
+	}
+
 }
