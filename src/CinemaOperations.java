4b0732e8ea5c4652a00191e132f89e1a0d836233
u1671506 <u1671506@CW203-07.AD.HUD.AC.UK>
2017-03-02 12:44:50 +0000

    cbndcbvm,c

--- src/CinemaOperations.java
+++ src/CinemaOperations.java
@@ -1,34 +1,11 @@
-import java.util.ArrayList;
+
 
 public class CinemaOperations {
 
-	Theater theater;
-	private ArrayList<Theater> theaters;	
-	private ArrayList<Snack> snacksList;
+	
 
 	public CinemaOperations() {
-		theaters = new ArrayList<Theater>();
-	}
-	/**
-	 * @param theater
-	 */
-	public void addTheater(Theater theater) {
-		theaters.add(theater);
+		;
 	}
 	
-	/**
-	 * @param snack
-	 */
-	public void addSnacks(Snack snack){
-		
-	}
-	/**
-	 * Get the list of Snacks sold at the theater
-	 * 
-	 * @return the snacksList
-	 */
-	public ArrayList<Snack> getSnacksList() {
-		return snacksList;
-	}
-
 }
